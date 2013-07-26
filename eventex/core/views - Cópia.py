bf81e3309f5c2774c0a2f3ccc47625e710365279
Paula <paula@escritoriovr.com.br>
2013-07-26 16:37:58 -0300

    All yout base are belong to us

---
+++ eventex/core/views - Cópia.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+from django.http import HttpResponse
+from django.template import loader, Context
+
+def homepage(request):
+	t = loader.get_template('index.html')
+	c = Context()
+	
+	content = t.render(c)
+	
+	return HttpResponse(content)
