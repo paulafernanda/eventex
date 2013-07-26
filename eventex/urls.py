9e8571233f718b3e223b3291aa74af15ee37e452
Paula <paula@escritoriovr.com.br>
2013-07-26 20:07:37 -0300

    All your base belong to us

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,4 +16,8 @@ urlpatterns = patterns('eventex.core.views',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('django.views.static',
+	url(r'^static/(?P<path>.*)$', 'serve', {'document_root': settings.STATIC_ROOT}),
 )
